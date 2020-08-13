ab64967e013967306fbdbe5c1d9278638cf5dd88
Darc99 <lennyyhugoh@gmail.com>
2020-08-13 23:15:38 +0100

    added parent tweet

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -4,6 +4,11 @@ import { formatTweet } from '../utils/helpers';
 
 class Tweet extends Component {
     render() {
+        const { tweet } = this.props;
+
+        if (tweet === null) {
+            return <p>This Tweet doesn't exist</p>
+        }
         console.log(this.props)
         return (
             <div className="tweet">
@@ -13,13 +18,25 @@ class Tweet extends Component {
     }
 }
 
-function mapStateToProps ({ authedUser, users, tweets }, { id }) {
-    const tweet = tweets[id]
+//function mapStateToProps ({ authedUser, users, tweets }, { id }) {
+//     const tweet = tweets[id]
+
+//     return {
+//         authedUser,
+//         tweet: formatTweet(tweet, users[tweet.author], authedUser)
+//     }
+// }
+
+function mapStateToProps ({authedUser, users, tweets}, { id }) {
+    const tweet = tweets[id];
+    const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
     return {
-        authedUser,
-        tweet: formatTweet(tweet, users[tweet.author], authedUser)
-    }
+      authedUser,
+      tweet: tweet 
+        ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+        : null
+    };
 }
 
 export default connect(mapStateToProps)(Tweet);
