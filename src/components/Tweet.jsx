f1a364db54e34504317d58db4354153622f65dfe
Darc99 <lennyyhugoh@gmail.com>
2020-08-24 00:25:52 +0100

    added router to tweet

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -4,6 +4,7 @@ import { formatTweet, formatDate } from '../utils/helpers';
 import { TiArrowBackOutline, 
     TiHeartFullOutline, TiHeartOutline } from 'react-icons/ti/index';
 import { handleToggleTweet } from '../actions/tweets';
+import { Link, withRouter } from 'react-router-dom';
 
 
 class Tweet extends Component {
@@ -20,6 +21,7 @@ class Tweet extends Component {
     }
     toParent = (e, id) => {
       e.preventDefault()
+      this.props.history.push(`/tweet/${id}`)
     }
     render() {
         const { tweet } = this.props;
@@ -29,11 +31,11 @@ class Tweet extends Component {
         }
         
         const { 
-            name, avatar, timestamp, text, hasLiked, likes, replies, parent
+            name, avatar, timestamp, text, hasLiked, likes, replies, id, parent
         } = tweet
 
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
               <img
                 src={avatar}
                 alt={`Avatar of ${name}`}
@@ -61,7 +63,7 @@ class Tweet extends Component {
                   <span>{likes !== 0 && likes}</span>
                 </div>
               </div>
-            </div>
+            </Link>
           )
     }
 }
@@ -78,4 +80,4 @@ function mapStateToProps ({authedUser, users, tweets}, { id }) {
     };
 }
 
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
